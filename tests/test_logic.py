9206bcee21f4e3752d1d3b1318b0e4863df44651
Approaching-Truth <202109174@post.au.dk>
2023-04-26 15:30:19 +0200

    test

--- tests/test_logic.py
+++ tests/test_logic.py
@@ -14,12 +14,12 @@ import logging
 
 
 
-sensor_1 = SensorRead("zigbee2mqtt/0x00158d000572a63f", 0)
+sensor_1 = SensorRead("zigbee2mqtt/0x00158d000572a63f", 0, "test.mosquitto.org")
 
 sensors = []
 sensors.append(sensor_1)
 
-led_1 = LightController("zigbee2mqtt/0xbc33acfffe8b8d7c/set", 0)
+led_1 = LightController("zigbee2mqtt/0xbc33acfffe8b8d7c/set", 0,"test.mosquitto.org")
 led_1.turnOff()
 lights = []
 lights.append(led_1)
