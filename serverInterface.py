b8b2ae43b4863bb798a99376ce9bdf7103181bf5
Approaching-Truth <202109174@post.au.dk>
2023-04-26 15:04:01 +0200

    test

--- serverInterface.py
+++ serverInterface.py
@@ -1,2 +1,17 @@
+import paho.mqtt.client as mqtt
+import json
+
 class Serverwriter:
-    x = 0
+    ### Initializes the connection to the light component
+    def __init__(self, message):
+        self.message = json.dumps(message)  
+
+        self.client = mqtt.Client()
+        self.client.connect("localhost", 1883)
+
+        self.serverIP = "127.0.0.1"
+
+    ### Turns on the light
+    def sendToServer(self):
+        print("Sending Data To Server")
+        self.client.publish(self.serverIP, self.message)  # Publishes to server
