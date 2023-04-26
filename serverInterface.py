e886a6f803a70e7dbcd09ab39a8d7c8ff4c2ac48
WokeCode <91264036+WokeCode@users.noreply.github.com>
2023-04-26 15:13:33 +0200

    Server_Test

--- serverInterface.py
+++ serverInterface.py
@@ -5,12 +5,12 @@ class Serverwriter:
     ### Initializes the connection to the light component
     def __init__(self):
         self.client = mqtt.Client()
-        self.client.connect("localhost", 1883)
+        self.client.connect("192.168.204.52", 1883)
 
-        self.serverIP = "127.0.0.1"
+        #self.serverIP = "127.0.0.1"
 
     ### Turns on the light
     def sendToServer(self, message):
         self.message = json.dumps(message)  
         print("Sending Data To Server")
-        self.client.publish(self.serverIP, self.message)  # Publishes to server
+        self.client.publish("TEST", self.message)  # Publishes to server
